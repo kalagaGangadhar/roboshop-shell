32534a2acdfdd3a071c6c1c85d08716379a2a1c4
Gangadhar <gangadharkalaga@gmail.com>
2023-12-18 19:03:31 +0530

    colors heading

--- catalogue.sh
+++ catalogue.sh
@@ -1,34 +1,34 @@
-echo ">>>>>>>>> create catalogue service <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> create catalogue service <<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo ">>>>>>>>> create mongo repo <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> create mongo repo <<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo ">>>>>>>>> disable default nodejs <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> disable default nodejs <<<<<<<<<\e[0m"
 dnf module disable nodejs -y
-echo ">>>>>>>>> enable nodejs18 <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> enable nodejs18 <<<<<<<<<\e[0m"
 dnf module enable nodejs:18 -y
-echo ">>>>>>>>> install nodejs <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> install nodejs <<<<<<<<<\e[0m"
 dnf install nodejs -y
-echo ">>>>>>>>> add application user <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> add application user <<<<<<<<<\e[0m"
 useradd roboshop
-echo ">>>>>>>>> remove app directory <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> remove app directory <<<<<<<<<\e[0m"
 rm -rf /app
-echo ">>>>>>>>> making app directory <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> making app directory <<<<<<<<<\e[0m"
 mkdir /app
-echo ">>>>>>>>> download catalogue zip file <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> download catalogue zip file <<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-echo ">>>>>>>>> extract the catalogue zip file <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> extract the catalogue zip file <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
-echo ">>>>>>>>> install dependencies <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> install dependencies <<<<<<<<<\e[0m"
 cd /app
 npm install
-echo ">>>>>>>>> reload daemon service <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> reload daemon service <<<<<<<<<\e[0m"
 systemctl daemon-reload
-echo ">>>>>>>>> install mongodb shell <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> install mongodb shell <<<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
-echo ">>>>>>>>> load schema to mongodb <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> load schema to mongodb <<<<<<<<<\e[0m"
 mongo --host mongodb.groboshop.online </app/schema/catalogue.js
-echo ">>>>>>>>> enable catalogue service <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> enable catalogue service <<<<<<<<<\e[0m"
 systemctl enable catalogue
-echo ">>>>>>>>> restart catalogue service <<<<<<<<<<"
+echo -e "\e[35m>>>>>>>> restart catalogue service <<<<<<<<<\e[0m"
 systemctl restart catalogue
