3918fb7105abc52e287c7f5a0fbdf0f961b3476f
Gangadhar <gangadharkalaga@gmail.com>
2023-12-18 19:26:01 +0530

    colors heading

--- catalogue.sh
+++ catalogue.sh
@@ -1,34 +1,35 @@
+log=/tmp/roboshop.log
 echo -e "\e[35m>>>>>>>> create catalogue service <<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service>>/tmp/catalogue.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 echo -e "\e[35m>>>>>>>> create mongo repo <<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo>>/tmp/catalogue.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 echo -e "\e[35m>>>>>>>> disable default nodejs <<<<<<<<<\e[0m"
-dnf module disable nodejs -y>>/tmp/catalogue.log
+dnf module disable nodejs -y &>>${log}
 echo -e "\e[35m>>>>>>>> enable nodejs18 <<<<<<<<<\e[0m"
-dnf module enable nodejs:18 -y>>/tmp/catalogue.log
+dnf module enable nodejs:18 -y &>>${log}
 echo -e "\e[35m>>>>>>>> install nodejs <<<<<<<<<\e[0m"
-dnf install nodejs -y>>/tmp/catalogue.log
+dnf install nodejs -y &>>${log}
 echo -e "\e[35m>>>>>>>> add application user <<<<<<<<<\e[0m"
-useradd roboshop>>/tmp/catalogue.log
+useradd roboshop &>>${log}
 echo -e "\e[35m>>>>>>>> remove app directory <<<<<<<<<\e[0m"
-rm -rf /app>>/tmp/catalogue.log
+rm -rf /app &>>${log}
 echo -e "\e[35m>>>>>>>> making app directory <<<<<<<<<\e[0m"
-mkdir /app>>/tmp/catalogue.log
+mkdir /app &>>${log}
 echo -e "\e[35m>>>>>>>> download catalogue zip file <<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip>>/tmp/catalogue.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 echo -e "\e[35m>>>>>>>> extract the catalogue zip file <<<<<<<<<\e[0m"
-cd /app>>/tmp/catalogue.log
-unzip /tmp/catalogue.zip>>/tmp/catalogue.log
+cd /app &>>${log}
+unzip /tmp/catalogue.zip &>>${log}
 echo -e "\e[35m>>>>>>>> install dependencies <<<<<<<<<\e[0m"
-cd /app>>/tmp/catalogue.log
-npm install>>/tmp/catalogue.log
+cd /app &>>${log}
+npm install &>>${log}
 echo -e "\e[35m>>>>>>>> reload daemon service <<<<<<<<<\e[0m"
-systemctl daemon-reload>>/tmp/catalogue.log
+systemctl daemon-reload &>>${log}
 echo -e "\e[35m>>>>>>>> install mongodb shell <<<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y>>/tmp/catalogue.log
+dnf install mongodb-org-shell -y &>>${log}
 echo -e "\e[35m>>>>>>>> load schema to mongodb <<<<<<<<<\e[0m"
-mongo --host mongodb.groboshop.online </app/schema/catalogue.js>>/tmp/catalogue.log
+mongo --host mongodb.groboshop.online </app/schema/catalogue.js &>>${log}
 echo -e "\e[35m>>>>>>>> enable catalogue service <<<<<<<<<\e[0m"
-systemctl enable catalogue>>/tmp/catalogue.log
+systemctl enable catalogue &>>${log}
 echo -e "\e[35m>>>>>>>> restart catalogue service <<<<<<<<<\e[0m"
-systemctl restart catalogue>>/tmp/catalogue.log
+systemctl restart catalogue &>>${log}
