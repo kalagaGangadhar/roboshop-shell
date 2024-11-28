d1303007e2efe66f01ec11a507bb2c8e460b2450
Gangadhar <gangadharkalaga@gmail.com>
2024-11-28 23:17:26 +0530

    shell scripting NFR

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,28 @@
+source common.sh
+
 echo -e "\e[36m>>>>>>>>>>>>  Install Nginx   <<<<<<<<<<<<\e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>${log}
+func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>  Copy RoboShop Configuration  <<<<<<<<<<<<\e[0m"
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
+func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>  Clean Old content  <<<<<<<<<<<<\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
+func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>  Download Application Content   <<<<<<<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
+func_exit_status
+cd /usr/share/nginx/html &>>${log}
+
 
 echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
+func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>  Start Nginx Service  <<<<<<<<<<<<\e[0m"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>>${log}
+systemctl start nginx &>>${log}
+func_exit_status
